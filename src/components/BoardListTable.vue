d8375fc8c2066bdb2e983cc37733d4730903ee5e
JH <dev.son.jh@gmail.com>
2024-04-30 22:19:25 +0900

    feat: added board files.

---
+++ src/components/BoardListTable.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { defineProps } from 'vue'
+const props = defineProps(['boardList'])
+</script>
+<template>
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Author</th>
+        <th scope="col">Created Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="board of props.boardList" :key="board">
+        <th scope="row">{{ board.number }}</th>
+        <td>{{ board.title }}</td>
+        <td>{{ board.author }}</td>
+        <td>{{ board.createdDate }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ src/views/BoardListView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import BoardListTable from '../components/BoardListTable.vue'
+import boards from '@/data/boards.json'
+const boardList = ref(new Array())
+const searchBoardList = () => {
+  boardList.value = boards
+}
+</script>
+<template>
+  <div class="row mb-3">
+    <label class="col-1 col-form-label">
+      Title
+    </label>
+    <div class="col-10">
+      <input type="text" class="form-control" placeholder="Please enter your keyword(s) to search."
+        v-model="titleKeyword">
+    </div>
+    <div class="col-1">
+      <button type="button" class="btn btn-primary" @click="searchBoardList">Search</button>
+    </div>
+  </div>
+  <BoardListTable :boardList="boardList" />
+  <div class="row">
+    <button></button>
+  </div>
+</template>
