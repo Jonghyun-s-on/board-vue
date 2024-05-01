a555d5496ee72c095abeff2f60bd5de64051ac37
JH <dev.son.jh@gmail.com>
2024-05-01 15:58:07 +0900

    build: added vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate';
 import boards from './modules/boards'
 
 export default createStore({
   modules: { boards },
+  plugins: [
+    createPersistedState()
+  ]
 })
