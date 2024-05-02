e87c7e15b19e2eab2a40cd2e318cf0f1c6ba5847
JH <dev.son.jh@gmail.com>
2024-05-02 21:57:09 +0900

    feat: added comment list table, writing a comment.

---
+++ src/components/CommentListTable.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(['commentList'])
+</script>
+
+<template>
+  <div v-for="comment of props.commentList" :key="comment.commentNumber" class="row mb-2">
+    <div class="col-2">
+      <input class="form-control text-center" :value="comment.author" readonly>
+    </div>
+    <div class="col-7">
+      <textarea class="form-control" :value="comment.comment" readonly rows="1"></textarea>
+    </div>
+    <div class="col-2">
+      <input class="form-control" :value="comment.createdDate" readonly>
+    </div>
+    <div class="col-1">
+      <div class="row">
+        <div class="col-6">
+          <button type="button" class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+        </div>
+        <div class="col-6">
+          <button type="button" class="btn btn-outline-danger"><i class="bi bi-trash"></i></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/BoardViewView.vue
+++ src/views/BoardViewView.vue
@@ -1,15 +1,21 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, getCurrentInstance } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import { useStore } from 'vuex'
+import CommentListTable from '@/components/CommentListTable.vue'
 // import { fetchJsonFile } from '@/util/JsonFileUtil'
 onMounted(() => {
-  searchBoard()
+  searchBoard(route.query.number)
 })
+const { proxy } = getCurrentInstance()
 const router = useRouter()
 const route = useRoute()
 const store = useStore()
 const board = ref(new Object())
+const commentList = ref(new Array())
+const author = ref(new String())
+const comment = ref(new String())
+const password = ref(new String())
 // let originBoard = new Object()
 // const props = defineProps(['number']) // be active when 'props: true' on router/index.js
 // const searchBoard = async () => {
@@ -21,50 +27,116 @@ const board = ref(new Object())
 //     }
 //   })
 // }
-const searchBoard = async () => {
-  let selectedBoard = store.getters.getBoard(route.query.number)
+const searchBoard = (num) => {
+  let selectedBoard = store.getters.getBoard(num)
   board.value = selectedBoard
+  commentList.value = searchComments(num)
 }
-const moveToWrite = () => {
+
+const searchComments = (num) => {
+  return store.getters.getComments(num)
+}
+
+const moveToWrite = (num) => {
   router.push({
     name: 'boardWrite',
-    params: { number: route.query.number }
+    params: { number: num }
   })
 }
+
 const moveBack = () => {
   router.go(-1)
+}
+
+const saveComment = (num) => {
+  if (author.value == '') {
+    alert('author empty!')
+    proxy.$refs.authorRef.focus() // focus on author input
+    return false
+  }
+  if (comment.value == '') {
+    alert('comment empty!')
+    proxy.$refs.commentRef.focus() // focus on comment input
+    return false
+  }
+  if (password.value == '') {
+    alert('password empty!')
+    proxy.$refs.passwordRef.focus() // focus on password input
+    return false
+  }
+  store.dispatch('insertComment', {
+    boardNumber: num,
+    author: author.value,
+    comment: comment.value,
+    password: password.value,
+  })
+  commentList.value = searchComments(num)
 }
 </script>
 
 <template>
-  <div class="row">
-    <label class="col-2 col-form-label">Author</label>
-    <div class="col-2">
-      <input type="text" class="form-control-plaintext" v-model="board.author" readonly>
+  <div class="mb-5">
+    <div class="row">
+      <label class="col-2 col-form-label">Author</label>
+      <div class="col-2">
+        <input type="text" class="form-control-plaintext" v-model="board.author" readonly>
+      </div>
+    </div>
+    <div class="row mb-3">
+      <label class="col-2 col-form-label">Created Date</label>
+      <div class="col-2">
+        <input type="text" class="form-control-plaintext" v-model="board.createdDate" readonly>
+      </div>
+      <label class="col-2 col-form-label">Modified Date</label>
+      <div class="col-2">
+        <input type="text" class="form-control-plaintext" v-model="board.modifiedDate" readonly>
+      </div>
+    </div>
+    <div class="mb-3">
+      <label class="form-label">Title</label>
+      <input type="text" class="form-control" v-model="board.title" readonly>
+    </div>
+    <div class="mb-3">
+      <label class="form-label">Content</label>
+      <textarea class="form-control" rows="3" v-model="board.content" readonly></textarea>
+    </div>
+    <div class="row">
+      <div>
+        <button type="button" class="col-1 btn btn-primary" @click="moveToWrite(board.boardNumber)">Modify</button>
+        <button type="button" class="col-1 offset-10 btn btn-danger" @click="moveBack">Back</button>
+      </div>
     </div>
   </div>
   <div class="row mb-3">
-    <label class="col-2 col-form-label">Created Date</label>
-    <div class="col-2">
-      <input type="text" class="form-control-plaintext" v-model="board.createdDate" readonly>
-    </div>
-    <label class="col-2 col-form-label">Modified Date</label>
-    <div class="col-2">
-      <input type="text" class="form-control-plaintext" v-model="board.modifiedDate" readonly>
-    </div>
+    <p class="h5">Comments</p>
   </div>
-  <div class="mb-3">
-    <label class="form-label">Title</label>
-    <input type="text" class="form-control" v-model="board.title" readonly>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Content</label>
-    <textarea class="form-control" rows="3" v-model="board.content" readonly></textarea>
+  <div class="row mb-5">
+    <CommentListTable :commentList="commentList" />
   </div>
   <div class="row">
-    <div>
-      <button type="button" class="col-1 btn btn-primary" @click="moveToWrite">Modify</button>
-      <button type="button" class="col-1 offset-10 btn btn-danger" @click="moveBack">Back</button>
+    <div class="col-2">
+      <div class="mb-3">
+        <label class="form-label">Author</label>
+        <input type="text" class="form-control" placeholder="Author" v-model="author" ref="authorRef">
+      </div>
+    </div>
+    <div class="col-8">
+      <div class="mb-3">
+        <label class="form-label">Comment</label>
+        <input type="text" class="form-control" placeholder="Please enter your comment here!" v-model="comment"
+          ref="commentRef">
+      </div>
+    </div>
+    <div class="col-2">
+      <div class="mb-3">
+        <label class="form-label">Password</label>
+        <input type="password" class="form-control" placeholder="Password" v-model="password" ref="passwordRef">
+      </div>
+    </div>
+    <div class="row">
+      <div>
+        <button type="button" class="col-1 btn btn-primary" @click="saveComment(board.boardNumber)">Save</button>
+      </div>
     </div>
   </div>
 </template>
